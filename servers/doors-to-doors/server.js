0d8432947b4a3795e33bf95e3b46f9b5ba613995
lizz thabet <lizzthabet@proton.me>
2025-05-03 14:54:40 -0400

    refactor routing on trickster server to make html files valid when not being served

--- servers/doors-to-doors/server.js
+++ servers/doors-to-doors/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const url = require("url");
 const { readdirSync, readFileSync } = fs;
 const { join, parse } = path;
 
 // Constants
 const SERVER_PORT = process.env.PORT || 2030;
-const PUBLIC_DIR = "/public";
-const ASSETS_FILE = ".glitch-assets";
+// Set this variable if there is a list of CDN files on disk
+const CDN_ASSETS_FILE = "";
+// This is where files will be listed and cached for responses
+const PUBLIC_DIR = "public";
 // Add any files that you'd like actually served up by this server, no tricks!!
-const ACTUALLY_SERVE_FILENAMES_MATCHING = ["index.html", "door.html", "_draggable.js"];
+const ACTUALLY_SERVE_FILENAMES_MATCHING = new Set(["index.html", "door.html", "_draggable.js"]);
 
 /**
  * @typedef File
@@ -27,11 +28,17 @@ let publicFiles = undefined;
 /** @type ExtToFile */
 let publicFilesByExt = undefined;
 /** @type File[] */
-let assetFiles = undefined;
+let cdnFiles = undefined;
 /** @type ExtToFile */
-let assetFilesByExt = undefined;
+let cdnFilesByExt = undefined;
 
 // Helper functions
+/**
+ * PublicPath returns the full path on disk the PUBLIC_DIR directory
+ * or a file in the PUBLIC_DIR directory
+ * @param {string?} path The location of public file
+ * @returns {string} fullPath The full path to file
+ */
 function publicPath(path) {
   if (path) {
     return join(__dirname, PUBLIC_DIR, path);
@@ -40,14 +47,27 @@ function publicPath(path) {
   return join(__dirname, PUBLIC_DIR);
 }
 
+/**
+ * @param {string} string 
+ * @returns {string}
+ */
 function normalize(string) {
   return string.toLowerCase();
 }
 
+/**
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
 function random(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+/**
+ * @param {string} path
+ * @returns {boolean}
+ */
 function shouldIgnore(path) {
   const { ext } = parse(path);
   // Ignore any directories
@@ -58,9 +78,15 @@ function shouldIgnore(path) {
   return false;
 }
 
+/**
+ * ShouldActuallyServe checks the path filename against a list of
+ * assets it should serve without any tricks
+ * @param {string} path 
+ * @returns {boolean}
+ */
 function shouldActuallyServe(path) {
   const { base } = parse(path);
-  if (path === "/" || ACTUALLY_SERVE_FILENAMES_MATCHING.includes(base)) {
+  if (path === "/" || ACTUALLY_SERVE_FILENAMES_MATCHING.has(base)) {
     return true;
   }
 
@@ -83,6 +109,14 @@ function mapFilesToExt(files) {
   return extMap;
 }
 
+/**
+ * Looks up a random file from the supplied list,
+ * with a max number of tries in case there aren't
+ * enough files to select a random other one.
+ * @param {string} path
+ * @param {File[]} list
+ * @returns {File|undefined}
+ */
 function getRandomFile(path, list) {
   if (!list || list.length === 0) {
     return;
@@ -107,6 +141,11 @@ function getRandomFile(path, list) {
   return { name: `${selectedName}${ext}`, location: selectedPath };
 }
 
+/**
+ * ListPublicFiles lists all files in PUBLIC_DIR directory
+ * and returns a list of their names and paths.
+ * @returns {File[]} fileList
+ */
 function listPublicFiles() {
   try {
     const publicDir = publicPath();
@@ -122,38 +161,47 @@ function listPublicFiles() {
     return [];
   }
 }
-
-function listGlitchAssets() {
+/**
+ * Reads a json file on disk if ASSETS_FILE is set.
+ * The structure of this file should be a newline separated
+ * list of JSON objects with "name" and "url" fields.
+ * @returns {File[]} fileList
+ */
+function listCDNFiles() {
   /** @type File[] */
-  const filesList = [];
+  const fileList = [];
+  if (!CDN_ASSETS_FILE) {
+    return fileList
+  }
+
   try {
-    const assetPath = join(__dirname, ASSETS_FILE);
-    const glitchFiles = readFileSync(assetPath, { encoding: "utf-8" });
-    glitchFiles
+    const assetPath = join(__dirname, CDN_ASSETS_FILE);
+    const assetFiles = readFileSync(assetPath, { encoding: "utf-8" });
+    assetFiles
       .trim()
       .split("\n")
       .forEach((assetString) => {
         try {
           const asset = JSON.parse(assetString);
           if (asset.hasOwnProperty("name") && asset.hasOwnProperty("url")) {
-            filesList.push({ name: asset.name, location: asset.url });
+            fileList.push({ name: asset.name, location: asset.url });
           }
         } catch (err) {
           // Ignore any errors parsing individual JSON elements
           console.error(
-            `failed to parse glitch asset string "${assetString}" to json`,
+            `failed to parse asset string "${assetString}" to json`,
             err
           );
         }
       });
   } catch (err) {
-    console.error(`failed to list glitch assets from ${ASSETS_FILE}:`, err);
+    console.error(`failed to list assets from ${CDN_ASSETS_FILE}:`, err);
   }
 
-  return filesList;
+  return fileList;
 }
 
-function sendAsset(res, location) {
+function redirectToLocation(res, location) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET");
   res.header(
@@ -166,11 +214,24 @@ function sendAsset(res, location) {
 // Server and request handlers
 const app = express();
 
-// Handler for any Glitch assets, where the request is for
-// the path "assets/filename.jpg"
-app.use("/assets", (req, res, next) => {
+// Middleware to label each incoming request with whether the file
+// should be faithfully served
+app.use((req, _res, next) => {
+  const { base } = parse(req.path);
+  if (shouldActuallyServe(base)) {
+    req.shouldActuallyServe = true;
+  } else {
+    req.shouldActuallyServe = false;
+  }
+  next();
+})
+
+// Middleware for any assets served through a CDN, where the request for
+// an asset with the path "cdn/filename.jpg" is redirected to
+// the appropriate (or not appropriate ;)) resource
+app.use("/cdn", (req, res, next) => {
   const { path } = req;
-  if (shouldActuallyServe(path)) {
+  if (req.shouldActuallyServe) {
     next();
     return;
   }
@@ -178,30 +239,48 @@ app.use("/assets", (req, res, next) => {
   const { ext } = parse(path);
   const assetToSend = getRandomFile(
     decodeURI(path),
-    assetFilesByExt[normalize(ext)]
+    cdnFilesByExt[normalize(ext)]
   );
   if (assetToSend !== undefined) {
     console.log(`request for ${path} > 👻 > sending ${assetToSend.name}`);
-    sendAsset(res, assetToSend.location);
+    redirectToLocation(res, assetToSend.location);
   } else {
     console.warn(`request for ${path} > 👻 > sorry not found`);
-    res.status(404);
-    res.end();
+    res.sendStatus(404);
   }
   return;
 });
 
-// Handler for any files in the "/public" folder, which usually
-// includes js, css, html, and anything else not considered an asset
-// and request matches the path "public/some-page.html"
-app.use("/public", (req, res, next) => {
-  const { path } = req;
-  if (shouldActuallyServe(path)) {
-    next();
-    return;
+// Always serve the index file on "/"
+app.get("/", (_req, res) => {
+  res.sendFile(publicPath("index.html"));
+});
+
+// Getting to this wildcard route handler means that we
+// actually want to serve the requested file
+app.get("*", (req, res) => {
+  const { shouldActuallyServe, path } = req;
+  const { base, ext } = parse(path);
+  if (shouldActuallyServe) {
+    // TODO: Optimize for performance here
+    // Check all the files in memory to see if there's a match
+    const filesToCheck = [...cdnFiles, ...publicFiles];
+    const fileFound = filesToCheck.find((f) => f.name === base);
+    if (fileFound) {
+      if (fileFound.location.startsWith("https")) {
+        redirectToLocation(res, fileFound.location);
+        return;
+      } else {
+        res.sendFile(fileFound.location);
+        return;
+      }
+    } else {
+      console.warn(`request for ${path} > 👻 > sorry not found`);
+      res.sendStatus(404);
+      return;
+    }
   }
 
-  const { ext } = parse(path);
   const fileToSend = getRandomFile(
     decodeURI(path),
     publicFilesByExt[normalize(ext)]
@@ -213,35 +292,8 @@ app.use("/public", (req, res, next) => {
     return;
   } else {
     console.warn(`request for ${path} > 👻 > sorry not found`);
-    res.status(404);
-    res.end();
-  }
-});
-
-// Always serve the index file on "/"
-app.get("/", (_req, res) => {
-  res.sendFile(publicPath("index.html"));
-});
-
-// Getting to this wildcard route handler means that we
-// actually want to serve the requested file
-app.get("*", (req, res) => {
-  const { base } = parse(req.path);
-  // Check all the files in memory to see if there's a match
-  const filesToCheck = [...assetFiles, ...publicFiles];
-  const fileFound = filesToCheck.find((f) => f.name === base);
-  if (fileFound) {
-    if (fileFound.location.startsWith("https")) {
-      sendAsset(res, fileFound.location);
-      return;
-    } else {
-      res.sendFile(fileFound.location);
-      return;
-    }
-  } else {
-    console.warn(`request for ${path} > 👻 > sorry not found`);
     res.sendStatus(404);
-    res.end();
+    return;
   }
 });
 
@@ -251,14 +303,14 @@ app.listen(SERVER_PORT, () => {
 
 /**
  * The below code executes as soon as the server starts up
- * and makes a list of all the files in the `/public` directory
- * as well as CDN assets, stored in the `.glitch-assets` file
+ * and makes a list of all the files in the public directory
+ * as well as any assets listed in a static file
  */
 // Get a list of files from /public directory
 publicFiles = listPublicFiles();
 // Map that file list by ext type for easy access
 publicFilesByExt = mapFilesToExt(publicFiles);
-// Get a list of files from glitch assets
-assetFiles = listGlitchAssets();
+// Get a list of files from CDN assets list
+cdnFiles = listCDNFiles();
 // Map that file list by ext type for easy access
-assetFilesByExt = mapFilesToExt(assetFiles);
+cdnFilesByExt = mapFilesToExt(cdnFiles);
